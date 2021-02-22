c281ae56c3f841a2bdf9f42793733d6aa3d04366
Anthony Shaw <anthonyshaw1714@gmail.com>
2021-02-22 18:36:12 -0500

    Spinning

--- src/main/java/org/richyrich/tetris/SpinnyText.java
+++ src/main/java/org/richyrich/tetris/SpinnyText.java
@@ -4,8 +4,14 @@ import java.awt.*;
 
 public class SpinnyText {
     String str;
+    int strWidth;
     int y;
     int x;
+
+    //  Units: Pixels per second
+    int velocityX;
+    int velocityY;
+
     float rotSpeed; //number of rotations/second
     float theta;
 
@@ -19,6 +25,10 @@ public class SpinnyText {
         this.theta = theta;
         this.color = color;
         this.font = font;
+
+        this.strWidth = 0;
+        this.velocityX = 0;
+        this.velocityY = 150;
     }
 
     public void update(int elapsedTimeMillis, String str) {
@@ -30,8 +40,26 @@ public class SpinnyText {
         g.setColor(color);
         g.setFont(font);
         FontMetrics fontMetrics = g.getFontMetrics();
+        strWidth = fontMetrics.stringWidth(str);
         g.translate(x,y);
         g.rotate(theta); //g.rotate rotates around 0,0
-        g.drawString(str, 0 - (fontMetrics.stringWidth(str) / 2), 0);
+        g.drawString(str, 0 - (strWidth / 2), 0);
+    }
+
+    private void screenSaver(int elapsedTimeMillis, Dimension dim){
+        double textExtentX = Math.abs(strWidth/2 * Math.cos(theta));
+        double textExtentY = Math.abs(strWidth/2 * Math.sin(theta));
+
+        if((x - (textExtentX)) <= 0 || (x + (textExtentX)) >= dim.width){
+            velocityX = -velocityX;
+            rotSpeed = -rotSpeed;
+        }
+        if((y - (textExtentY)) <= 0 || (y + (textExtentY)) >= dim.height){
+            velocityY = -velocityY;
+            rotSpeed = -rotSpeed;
+        }
+
+        x += (velocityX * (elapsedTimeMillis/1000f));
+        y += (velocityY * (elapsedTimeMillis/1000f));
     }
 }
