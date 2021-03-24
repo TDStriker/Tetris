e33238ebc8ab3bf3a8688de5a5097d83dacc5951
Anthony Shaw <anthonyshaw1714@gmail.com>
2021-03-23 20:30:28 -0400

    Button

--- src/main/java/org/richyrich/tetris/utilities/MouseClickListener.java
+++ src/main/java/org/richyrich/tetris/utilities/MouseClickListener.java
@@ -4,6 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class MouseClickListener extends MouseAdapter {
+public class MouseClickListener implements MouseListener {
+    Object origin;
 
+    public Object getClickOrigin(){
+        return origin;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        origin = e.getSource();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
