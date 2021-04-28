40709a0883d578f4dc1baee72fadaa45bc21980d
Anthony Shaw <61851624+TDStriker@users.noreply.github.com>
2021-04-28 12:19:27 -0400

    Sound effects

--- src/main/java/org/richyrich/tetris/Sound.java
+++ src/main/java/org/richyrich/tetris/Sound.java
@@ -1,4 +1,21 @@
 package org.richyrich.tetris;
 
 public class Sound {
+    String path;
+    
+    public Sound(String filePath){
+        this.path = filePath;   
+    }
+    
+    public void playSound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(path).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
 }
