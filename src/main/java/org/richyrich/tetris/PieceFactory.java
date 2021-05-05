43c3ceb660591691c6d0039aa0918714520850dd
Anthony Shaw <61851624+TDStriker@users.noreply.github.com>
2021-05-04 21:06:10 -0400

    Update PieceFactory.java

--- src/main/java/org/richyrich/tetris/PieceFactory.java
+++ src/main/java/org/richyrich/tetris/PieceFactory.java
@@ -1,8 +1,12 @@
 package org.richyrich.tetris;
 
 import org.richyrich.tetris.utilities.CustomColor;
+import org.richyrich.tetris.utilities.CustomImage;
+import org.richyrich.tetris.utilities.TetrisSettings;
 
+import javax.imageio.ImageIO;
 import java.awt.Color;
+import java.io.File;
 
 public class PieceFactory {
     public static final PieceFactory DEFAULT = new PieceFactory();
@@ -36,60 +40,88 @@ public class PieceFactory {
 
     public TetrisPiece rhodeIsland() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(Color.yellow, 2, 0);
-        bobby[1] = new Block(Color.yellow, 1, 0);
-        bobby[2] = new Block(Color.yellow, 1, 1);
-        bobby[3] = new Block(Color.yellow, 0, 1);
+        try{
+            bobby[0] = new Block(Color.yellow, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 2, 0);
+            bobby[1] = new Block(Color.yellow, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 0);
+            bobby[2] = new Block(Color.yellow, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 1);
+            bobby[3] = new Block(Color.yellow, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 1);
+        }catch(Exception e){
+
+        }
         return new TetrisPiece(bobby);
     }
     public TetrisPiece clevlandZ() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(Color.orange, 0, 0);
-        bobby[1] = new Block(Color.orange, 1, 0);
-        bobby[2] = new Block(Color.orange, 1, 1);
-        bobby[3] = new Block(Color.orange, 2, 1);
+        try{
+            bobby[0] = new Block(Color.orange, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 0);
+            bobby[1] = new Block(Color.orange, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 0);
+            bobby[2] = new Block(Color.orange, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 1);
+            bobby[3] = new Block(Color.orange, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 2, 1);
+        }catch(Exception e){
+
+        }
         return new TetrisPiece(bobby);
     }
     public TetrisPiece cube() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(Color.green, 0, 0);
-        bobby[1] = new Block(Color.green, 0, 1);
-        bobby[2] = new Block(Color.green, 1, 0);
-        bobby[3] = new Block(Color.green, 1, 1);
+        try {
+        bobby[0] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 0);
+        bobby[1] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 1);
+        bobby[2] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 0);
+        bobby[3] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 1);
+    } catch (Exception e){
+
+        }
         TetrisPiece Robert = new TetrisPiece(bobby);
         Robert.setColorChanging(true);
         return Robert;
     }
     public TetrisPiece luigi() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(Color.green, 0, 0);
-        bobby[1] = new Block(Color.green, 0, 1);
-        bobby[2] = new Block(Color.green, 0, 2);
-        bobby[3] = new Block(Color.green, 1, 2);
+        try{
+            bobby[0] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 0);
+            bobby[1] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 1);
+            bobby[2] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 2);
+            bobby[3] = new Block(Color.green, new CustomImage (ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 2);
+        } catch(Exception e) {
+
+        }
         return new TetrisPiece(bobby);
     }
     public TetrisPiece waluigi() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(new Color(170,0,255), 1, 0);
-        bobby[1] = new Block(new Color(170,0,255), 1, 1);
-        bobby[2] = new Block(new Color(170,0,255), 1, 2);
-        bobby[3] = new Block(new Color(170,0,255), 0, 2);
+        try {
+            bobby[0] = new Block(new Color(170, 0, 255), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 0);
+            bobby[1] = new Block(new Color(170, 0, 255), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 1);
+            bobby[2] = new Block(new Color(170, 0, 255), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 2);
+            bobby[3] = new Block(new Color(170, 0, 255), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 2);
+        } catch(Exception e) {
+
+        }
         return new TetrisPiece(bobby);
     }
     public TetrisPiece triangleTryhard() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(Color.blue, 1, 0);
-        bobby[1] = new Block(Color.blue, 0, 1);
-        bobby[2] = new Block(Color.blue, 1, 1);
-        bobby[3] = new Block(Color.blue, 2, 1);
+        try {
+            bobby[0] = new Block(Color.blue, new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 0);
+            bobby[1] = new Block(Color.blue, new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 1);
+            bobby[2] = new Block(Color.blue, new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 1, 1);
+            bobby[3] = new Block(Color.blue, new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 2, 1);
+        } catch (Exception e) {
+
+        }
         return new TetrisPiece(bobby);
     }
     public TetrisPiece holyEye() {
         Block[] bobby = new Block[4];
-        bobby[0] = new Block(new CustomColor(255,0,0), 0, 0);
-        bobby[1] = new Block(new CustomColor(255,0,0), 0, 1);
-        bobby[2] = new Block(new CustomColor(255,0,0), 0, 2);
-        bobby[3] = new Block(new CustomColor(255,0,0), 0, 3);
+        try {
+            bobby[0] = new Block(new CustomColor(255, 0, 0), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 0);
+            bobby[1] = new Block(new CustomColor(255, 0, 0), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 1);
+            bobby[2] = new Block(new CustomColor(255, 0, 0), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 2);
+            bobby[3] = new Block(new CustomColor(255, 0, 0), new CustomImage(ImageIO.read(new File(TetrisSettings.blockImagePath))), 0, 3);
+        }catch(Exception e){
+
+        }
         TetrisPiece Robert = new TetrisPiece(bobby);
         Robert.setGradient(true);
         return Robert;
