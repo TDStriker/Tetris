ab0f60d785fda5b2cb52f6f10f693cf8bd4a89ff
Anthony Shaw <61851624+TDStriker@users.noreply.github.com>
2021-05-04 21:06:38 -0400

    Update Sound.java

--- src/main/java/org/richyrich/tetris/Sound.java
+++ src/main/java/org/richyrich/tetris/Sound.java
@@ -1,12 +1,17 @@
 package org.richyrich.tetris;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
 public class Sound {
     String path;
-    
+
     public Sound(String filePath){
-        this.path = filePath;   
+        this.path = filePath;
     }
-    
+
     public void playSound() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(path).getAbsoluteFile());
